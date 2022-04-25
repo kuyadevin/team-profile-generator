6c1728d67485e17d39d8a0364e4d5eb528e51e9c
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-25 08:03:38 -0700

    Added intern and engineer questions

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ const Intern = require('./lib/intern');
 //Empty array representing new team
 const team = [];
 
+// Employee questions
+
+const employeeCard = inquirer.prompt([
+    {
+        type: 'input',
+        message:
+    },
+])
+
 // manager question
 const managerCard = inquirer.prompt([
     {
@@ -31,6 +40,54 @@ const managerCard = inquirer.prompt([
         type: 'input',
         message: "What is the manager's office number?",
         name: 'officeNumber', 
+    }
+])
+
+// Engineer questions
+const engineerCard = inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is the engineer's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's employee ID?",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's e-mail address?",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What is the engineer's GitHub username?",
+        name: 'github', 
+    }
+])
+
+// Intern questions
+const internCard = inquirer.prompt([
+    {
+        type: 'input',
+        message: "What is your intern's name?",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "What is the intern's employee ID?",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "What is the intern's e-mail address?",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "What school does the intern attend?",
+        name: 'school', 
     }
 ])
 
