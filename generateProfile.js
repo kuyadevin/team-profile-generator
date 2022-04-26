95923ec9c12d43b6d39c98ac5bcca8f68eeb3d87
KuyaDevin <devinnguyen@ucsb.edu>
2022-04-25 19:27:31 -0700

    Added card for manager and engineer

--- generateProfile.js
+++ generateProfile.js
@@ -2,15 +2,29 @@ const makeAllTeam = team => {
 
 const makeManager = manager => {
     return `
-    
-    
+    <div class="card" style="width: 18rem;">
+     <div class="card-body">
+         <h5 class="card-title">Manager</h5>
+         <h6 class="name mb-2">${manager.name}</h6>
+         <p class="id">Employee ID Number: ${manager.id}</p>
+         <p class="email">E-Mail Address: ${manager.email}</p>
+         <p class="officeNumber">Office Number: ${manager.officeNumber}</p>
+     </div>
+    </div>   
     `
 }
 
 const makeEngineer = engineer => {
     return `
-    
-    
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">Engineer</h5>
+        <h6 class="name mb-2">${engineer.name}</h6>
+        <p class="id">Employee ID Number: ${engineer.id}</p>
+        <p class="email">E-Mail Address: ${engineer.email}</p>
+        <p class="gitHub">GitHub Username: ${manager.officeNumber}</p>
+    </div>
+   </div>     
     `
 }
 
